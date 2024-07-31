c3657e2f43b61c352832b551217f2a49ba864b74
Lukas Kompatscher <80333836+LukasKompatscher@users.noreply.github.com>
2024-07-31 15:54:34 +0200

    optimize image creation

--- lib/src/pdf_thumbnail.dart
+++ lib/src/pdf_thumbnail.dart
@@ -236,19 +236,8 @@ class _PdfThumbnailState extends State<PdfThumbnail> {
       return cachedImage;
     }
     try {
-      final document = await PdfDocument.openFile(filePath);
-      final page = document.pages[pageNumber];
-      final aspectRatio = page.width / page.height;
-      final newHeight = 250;
-      final newWidth = (aspectRatio * newHeight).toInt();
-
-      final pageImage = await page.render(
-        width: newWidth,
-        height: newHeight,
-      );
-      final image = await pageImage!.createImage();
-      final pngBytes = await image.toByteData(format: ImageByteFormat.png);
-      final imageBytes = Uint8List.view(pngBytes!.buffer);
+      final imageBytes = await compute(
+          _createThumbnail, {'filePath': filePath, 'pageNumber': pageNumber});
       await cacher.write(filePath, pageNumber, imageBytes);
       _imageCache[pageNumber] = imageBytes;
       return imageBytes;
@@ -258,6 +247,20 @@ class _PdfThumbnailState extends State<PdfThumbnail> {
       }
       return null;
     }
+  }
+
+  Future<Uint8List> _createThumbnail(Map<String, dynamic> args) async {
+    final filePath = args['filePath'];
+    final pageNumber = args['pageNumber'];
+    final document = await PdfDocument.openFile(filePath);
+    final page = document.pages[pageNumber];
+    final pageImage = await page.render(
+      width: page.width.toInt(),
+      height: page.height.toInt(),
+    );
+    final image = await pageImage!.createImage();
+    final pngBytes = await image.toByteData(format: ImageByteFormat.png);
+    return Uint8List.view(pngBytes!.buffer);
   }
 
   @override
